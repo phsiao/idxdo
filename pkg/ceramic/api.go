148b9538d9b88897048e7d7cdaa7520b17795345
Shawn Hsiao <shawn.hsiao@gmail.com>
2022-06-18 15:49:20 -0400

    update endpoints

--- pkg/ceramic/api.go
+++ pkg/ceramic/api.go
@@ -1,7 +1,9 @@
 package ceramic
 
 const (
-	GITCOIN_PASSPORT_CERAMIC_ENDPOINT = "ceramic.passport-iam.gitcoin.co"
+	GITCOIN_PASSPORT_CERAMIC_ENDPOINT  = "ceramic.passport-iam.gitcoin.co"
+	MAINNET_COMMUNITY_CERAMIC_ENDPOINT = "gateway.ceramic.network"
+	CLAY_COMMUNITY_CERAMIC_ENDPOINT    = "gateway-clay.ceramic.network"
 )
 
 // API interacts with Ceramic API gateway
